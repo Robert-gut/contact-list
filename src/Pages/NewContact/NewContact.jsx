849a9ee91b8833461d57c8ab35103fd03b279273
Robert-gut <71551473+Robert-gut@users.noreply.github.com>
2023-05-03 14:23:36 +0300

    page add new contact

--- src/Pages/NewContact/NewContact.jsx
+++ src/Pages/NewContact/NewContact.jsx
@@ -3,7 +3,22 @@ import './NewContact.css'
 
 const NewContact = () => {
     return(
-        <h1>NewContact</h1>
+
+      <div className='container'>
+          <div className="modal-content rounded addPage shadow-sm">
+            <div className="modal-header">
+              <h1 className="text-center">Add new contact</h1>
+            </div>
+            <div className="modal-body">
+              
+              <button className="btn btn-primary btn-lg">
+                Add
+              </button>
+            </div>
+          </div>
+      </div>
+
+
     )
 }
 
