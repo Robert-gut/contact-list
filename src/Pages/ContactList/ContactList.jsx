fc1abf540b372d7165e4354d6d5d80be2c62f734
Robert-gut <71551473+Robert-gut@users.noreply.github.com>
2023-05-03 12:22:15 +0300

    Component ContactList with Sidebar, ContactItem

--- src/Pages/ContactList/ContactList.jsx
+++ src/Pages/ContactList/ContactList.jsx
@@ -1,9 +1,23 @@
+// css
 import './ContactList.css'
+
+//import components
+import Sidebar from '../../Componets/Sidebar/Sidebar'
+import ContactItem from '../../Componets/ContactItem/ContactItem'
 
 
 const ContactList = () => {
     return(
-        <h1>ContactList</h1>
+      <main className='container rounded'>
+      <div className='row'>
+        <div className='col-4'>
+          <Sidebar/>
+        </div>
+        <div className='col-8'>
+          <ContactItem/>
+        </div>
+      </div>
+    </main>
     )
 }
 
