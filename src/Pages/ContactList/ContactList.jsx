42a5736989ee3272b1181430e3643277ffa4c053
Robert-gut <71551473+Robert-gut@users.noreply.github.com>
2023-05-10 13:31:38 +0300

    added modefied ContactItem

--- src/Pages/ContactList/ContactList.jsx
+++ src/Pages/ContactList/ContactList.jsx
@@ -10,11 +10,11 @@ const ContactList = ({ stor }) => {
     return(
       <main className='container rounded'>
       <div className='row'>
-        <div className='col-3'>
-          <Sidebar/>
+        <div className='p-0 col-3'>
+          <Sidebar />
         </div>
-        <div className='col-9'>
-          <ContactItem stor={stor}/>
+        <div className='p-0 col-9'>
+          <ContactItem  stor={stor}/>
         </div>
       </div>
     </main>
